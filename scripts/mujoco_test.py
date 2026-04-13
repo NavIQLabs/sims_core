e4d99302dd478c70b5cbd368020057743d524824
AlphaGotReal <subhampatra2411@gmail.com>
2026-04-13 16:06:38 +0530

    xml file

---
+++ scripts/mujoco_test.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import mujoco
+import mujoco.viewer
+import time
+import numpy as np
+
+xml_path = os.path.abspath(sys.argv[1])
+os.chdir(os.path.dirname(xml_path))
+
+model = mujoco.MjModel.from_xml_path(xml_path)
+data = mujoco.MjData(model)
+
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    start_time = time.time()
+
+    while viewer.is_running() and time.time() - start_time < 30:
+        step_start = time.time()
+
+        t = data.time
+        data.ctrl[0] = np.sin(t) * 1.0
+        data.ctrl[1] = np.cos(t) * 0.5
+
+        mujoco.mj_step(model, data)
+
+        viewer.sync()
+
+        time_until_next_step = model.opt.timestep - (time.time() - step_start)
+        if time_until_next_step > 0:
+            time.sleep(time_until_next_step)
